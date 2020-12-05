9b5fee603cd1dc28451d8cac4400a8c7571d4cc3
Malachi <malachialusa@gmail.com>
2020-12-05 16:25:43 -0700

    updated color logic. Completed storage, and loading system for local data, formated text.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,25 @@ $("#currentDay").html(timenow);
 
 //getting  hour date month year from current time  
 var currenthour = moment().format("HH");
-console.log(currenthour);
 
-var currentday = moment().format("D")
-console.log(currentday);
+
+var currentday = moment().format("D");
+
 
 var currentmonth =moment().format("M");
-console.log(currentmonth);
+
 
 var currentyear = moment().format("YYYY");
-console.log(currentyear);
+
 
 var daystart = moment().startOf('date');
 
-//declaring to start of day from currenttime  
+var hourofdaystart =moment(daystart).format("HH")
 
+var timenowhour =moment().format("HH")
+
+
+//declaring to start of day from currenttime  
 
 
 
@@ -30,42 +34,52 @@ var daystart = moment().startOf('date');
 
 //9AM
 var nineblock = moment(daystart).add(9,'hours');
-nineblock =moment(nineblock).format("dddd, MMMM Do YYYY, h a");
+nineblock = moment(nineblock).format("HH");
+
 
 //color logic
-if(timenow < nineblock){
+if(timenow > nineblock){
     document.getElementById("9").classList.remove("present")
+    document.getElementById("9").classList.remove("future")
     document.getElementById("9").classList.add("past")
+    
 }
 
-if(timenow == fiveblock){
+if(timenow == nineblock){
     document.getElementById("9").classList.remove("future")
     document.getElementById("9").classList.add("present")
+    
  }
 
- if(timenow > fiveblock){
+ if(timenow < nineblock){
     document.getElementById("9").classList.remove("present")
-    document.getElementById("9").classList.add("past")
+    document.getElementById("9").classList.remove("past")
+    document.getElementById("9").classList.add("future")
+    
  }
 
-//10AM
-var tenblock = moment(daystart).add(10,'hours');
- tenblock =moment(tenblock).format("dddd, MMMM Do YYYY, h a");
+   //10AM
+   var tenblock = moment(daystart).add(10,'hours');
+   tenblock = moment(tenblock).format("dddd, MMMM Do YYYY, h a");
 
-//color logic
-if(timenow < tenblock){
+   //color logic
+   if(timenow > tenblock){
+    document.getElementById("10").classList.remove("future")
     document.getElementById("10").classList.remove("present")
     document.getElementById("10").classList.add("past")
-}
+    
+   }
 
-if(timenow == tenblock){
+   if(timenow == tenblock){
     document.getElementById("10").classList.remove("future")
     document.getElementById("10").classList.add("present")
- }
+   } 
 
- if(timenow > tenblock){
+ if(timenow < tenblock){
     document.getElementById("10").classList.remove("present")
-    document.getElementById("10").classList.add("past")
+    document.getElementById("10").classList.remove("past")
+    document.getElementById("10").classList.add("future")
+   
  }
 
  //11AM
@@ -73,7 +87,8 @@ if(timenow == tenblock){
  elevenblock =moment(elevenblock).format("dddd, MMMM Do YYYY, h a");
 
  //color logic
-if(timenow < elevenblock){
+if(timenow > elevenblock){
+    document.getElementById("11").classList.remove("future")
     document.getElementById("11").classList.remove("present")
     document.getElementById("11").classList.add("past")
 }
@@ -83,9 +98,10 @@ if(timenow == elevenblock){
     document.getElementById("11").classList.add("present")
  }
 
- if(timenow > elevenblock){
+ if(timenow < elevenblock){
     document.getElementById("11").classList.remove("present")
-    document.getElementById("11").classList.add("past")
+    document.getElementById("11").classList.remove("past")
+    document.getElementById("11").classList.add("future")
  }
 
  //12PM
@@ -93,7 +109,8 @@ if(timenow == elevenblock){
  noonblock =moment(noonblock).format("dddd, MMMM Do YYYY, h a");
 
  //color logic
-    if(timenow < noonblock){
+    if(timenow > noonblock){
+    document.getElementById("12").classList.remove("future")
     document.getElementById("12").classList.remove("present")
     document.getElementById("12").classList.add("past")
     }
@@ -103,9 +120,10 @@ if(timenow == elevenblock){
     document.getElementById("12").classList.add("present")
  }
 
-    if(timenow > noonblock){
+    if(timenow < noonblock){
     document.getElementById("12").classList.remove("present")
-    document.getElementById("12").classList.add("past")
+    document.getElementById("12").classList.remove("past")
+    document.getElementById("12").classList.add("future")
     }
 
  //1pm
@@ -113,7 +131,8 @@ if(timenow == elevenblock){
  oneblock =moment(oneblock).format("dddd, MMMM Do YYYY, h a");
 
  //color logic
-    if(timenow < oneblock){
+    if(timenow > oneblock){
+    document.getElementById("1").classList.remove("future")
     document.getElementById("1").classList.remove("present")
     document.getElementById("1").classList.add("past")
     }
@@ -123,9 +142,10 @@ if(timenow == elevenblock){
     document.getElementById("1").classList.add("present")
  }
 
-    if(timenow > oneblock){
+    if(timenow < oneblock){
     document.getElementById("1").classList.remove("present")
-    document.getElementById("1").classList.add("past")
+    document.getElementById("1").classList.remove("past")
+    document.getElementById("1").classList.add("future")
     }
 
     //2pm
@@ -133,7 +153,8 @@ if(timenow == elevenblock){
     twoblock =moment(twoblock).format("dddd, MMMM Do YYYY, h a");
 
     //color logic
-    if(timenow < twoblock){
+    if(timenow > twoblock){
+    document.getElementById("2").classList.remove("future")
     document.getElementById("2").classList.remove("present")
     document.getElementById("2").classList.add("past")
     }
@@ -143,9 +164,10 @@ if(timenow == elevenblock){
     document.getElementById("2").classList.add("present")
     }
 
-    if(timenow > twoblock){
+    if(timenow < twoblock){
         document.getElementById("2").classList.remove("present")
-        document.getElementById("2").classList.add("past")
+        document.getElementById("2").classList.remove("past")
+        document.getElementById("2").classList.add("future")
     }
 
     //3PM
@@ -153,8 +175,9 @@ if(timenow == elevenblock){
     threeblock =moment(threeblock).format("dddd, MMMM Do YYYY, h a");
 
         //color logic
-        if(timenow < threeblock){
-        document.getElementById("3").classList.remove("present")
+        if(timenow > threeblock){
+        document.getElementById("3").classList.remove("future")
+        document.getElementById("2").classList.remove("present")
         document.getElementById("3").classList.add("past")
         }
     
@@ -163,9 +186,11 @@ if(timenow == elevenblock){
         document.getElementById("3").classList.add("present")
         }
     
-        if(timenow > threeblock){
+        if(timenow < threeblock){
             document.getElementById("3").classList.remove("present")
-            document.getElementById("3").classList.add("past")
+            document.getElementById("3").classList.remove("past")
+            document.getElementById("3").classList.add("future")
+            
         }
 
     //4PM
@@ -173,19 +198,24 @@ if(timenow == elevenblock){
     fourblock =moment(fourblock).format("dddd, MMMM Do YYYY, h a");
 
       //color logic
-      if(timenow < fourblock){
+      if(timenow > fourblock){
+        document.getElementById("4").classList.remove("future")
         document.getElementById("4").classList.remove("present")
         document.getElementById("4").classList.add("past")
+        
         }
     
         if(timenow == fourblock){
         document.getElementById("4").classList.remove("future")
         document.getElementById("4").classList.add("present")
+        
         }
     
-        if(timenow > fourblock){
+        if(timenow < fourblock){
             document.getElementById("4").classList.remove("present")
-            document.getElementById("4").classList.add("past")
+            document.getElementById("4").classList.remove("past")
+            document.getElementById("4").classList.add("future")
+            
         }
 
 
@@ -196,9 +226,10 @@ fiveblock = moment(fiveblock).format("dddd, MMMM Do YYYY, h a");
     
 
     //color logic
-if(timenow < fiveblock){
-    document.getElementById("5").classList.remove("present")
-    document.getElementById("5").classList.add("past")
+if(timenow > fiveblock){
+    document.getElementById("5").classList.remove("past")
+    document.getElementById("5").classList.remove("future")
+    document.getElementById("5").classList.add("future")
  }
  
  if(timenow == fiveblock){
@@ -206,9 +237,10 @@ if(timenow < fiveblock){
     document.getElementById("5").classList.add("present")
  }
 
- if(timenow > fiveblock){
+ if(timenow < fiveblock){
     document.getElementById("5").classList.remove("present")
-    document.getElementById("5").classList.add("past")
+    document.getElementById("5").classList.remove("past")
+    document.getElementById("5").classList.add("future")
  }
 
  //6PM
@@ -216,9 +248,9 @@ if(timenow < fiveblock){
  sixblock = moment(sixblock).format("dddd, MMMM Do YYYY, h a");
 
  //color logic
- if(timenow < sixblock){
-    document.getElementById("6").classList.remove("present")
-    document.getElementById("6").classList.add("past")
+ if(timenow > sixblock){
+    document.getElementById("6").classList.remove("past")
+    document.getElementById("6").classList.add("future")
  }
 
  if(timenow == sixblock){
@@ -226,9 +258,10 @@ if(timenow < fiveblock){
     document.getElementById("6").classList.add("present")
  }
 
- if(timenow > sixblock){
+ if(timenow < sixblock){
     document.getElementById("6").classList.remove("present")
-    document.getElementById("6").classList.add("past")
+    document.getElementById("6").classList.remove("past")
+    document.getElementById("6").classList.add("future")
  }
 
 
@@ -236,28 +269,379 @@ if(timenow < fiveblock){
 
 
 
+//EVENT LISTENERS FOR HOURS ON SHEETS
 
-
+//9AM Listener
  $("#9").on("click","p",function(){
-    var text = $(this)
-    .text()
-    .trim();
-    var textInput = $("<textarea>")
-  .addClass("form-control")
-  .val(text);
+    var text = $(this).text().trim();
+
+    var textInput = $("<textarea>").val(text);      /*.addClass("form-control")  removed after ":<textarea>"*/
+
+   $(this).replaceWith(textInput);
+   textInput.trigger("focus");
+
+
+   $("#9").on("blur", "textarea", function(){
+
+     
+      var gettext =$("textarea").val();
+      text =gettext
+
+   
+   //sets up paragraph
+    var taskP = $("<p>").text(text);
+    $(this).replaceWith(taskP);
+   });
+
+   $("#9am").on("click", function(){
+      window.localStorage.setItem("9am",text)
+
+   })
+
+
+
+});
+
+
+//10AM Listener
+$("#10").on("click","p",function(){
+   var text = $(this).text().trim();
+
+   var textInput = $("<textarea>").val(text);      /*.addClass("form-control")  removed after ":<textarea>"*/
+
   $(this).replaceWith(textInput);
   textInput.trigger("focus");
 
-  $(this).val()
+
+  $("#10").on("blur", "textarea", function(){
+
+     //gets text, and rewrites it to text
+     var gettext =$("textarea").val();
+     text =gettext
+
+  
+  //sets up paragraph
+   var taskP = $("<p>").text(text);
+   $(this).replaceWith(taskP);
+
+   $("#10am").on("click", function(){
+      window.localStorage.setItem("10am",text)
+
+   })
 
 
- $("#9").on("blur", "textarea", function(){
-     
-     var taskP = $("<p>")
-  .addClass("m-1")
-  .text(text);
+  });
 
-// replace textarea with p element
-$(this).replaceWith(taskP);
- });
 });
+
+//11AM Listener
+$("#11").on("click","p",function(){
+   var text = $(this).text().trim();
+
+   var textInput = $("<textarea>").val(text);      /*.addClass("form-control")  removed after ":<textarea>"*/
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+
+
+  $("#11").on("blur", "textarea", function(){
+
+     //gets text, and rewrites it to text
+     var gettext =$("textarea").val();
+     text =gettext
+
+  
+  //sets up paragraph
+   var taskP = $("<p>").text(text);
+   $(this).replaceWith(taskP);
+
+   $("#11am").on("click", function(){
+      window.localStorage.setItem("11am",text)
+
+   })
+
+  });
+
+});
+
+
+//12PM Listener
+$("#12").on("click","p",function(){
+   var text = $(this).text().trim();
+
+   var textInput = $("<textarea>").val(text);      /*.addClass("form-control")  removed after ":<textarea>"*/
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+
+
+  $("#12").on("blur", "textarea", function(){
+
+     //gets text, and rewrites it to text
+     var gettext =$("textarea").val();
+     text =gettext
+
+  
+  //sets up paragraph
+   var taskP = $("<p>").text(text);
+   $(this).replaceWith(taskP);
+
+   $("#12pm").on("click", function(){
+      window.localStorage.setItem("12pm",text)
+
+   })
+
+  });
+
+});
+
+
+//1PM Listener
+$("#1").on("click","p",function(){
+   var text = $(this).text().trim();
+
+   var textInput = $("<textarea>").val(text);      /*.addClass("form-control")  removed after ":<textarea>"*/
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+
+
+  $("#1").on("blur", "textarea", function(){
+
+     //gets text, and rewrites it to text
+     var gettext =$("textarea").val();
+     text =gettext
+
+  
+  //sets up paragraph
+   var taskP = $("<p>").text(text);
+   $(this).replaceWith(taskP);
+
+   $("#1pm").on("click", function(){
+      window.localStorage.setItem("1pm",text)
+
+   })
+
+  });
+
+});
+
+
+//2PM Listener
+$("#2").on("click","p",function(){
+   var text = $(this).text().trim();
+
+   var textInput = $("<textarea>").val(text);      /*.addClass("form-control")  removed after ":<textarea>"*/
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+
+
+  $("#2").on("blur", "textarea", function(){
+
+     //gets text, and rewrites it to text
+     var gettext =$("textarea").val();
+     text =gettext
+
+  
+  //sets up paragraph
+   var taskP = $("<p>").text(text);
+   $(this).replaceWith(taskP);
+
+   $("#2pm").on("click", function(){
+      window.localStorage.setItem("2pm",text)
+
+   })
+
+  });
+
+});
+
+
+//3PM Listener
+$("#3").on("click","p",function(){
+   var text = $(this).text().trim();
+
+   var textInput = $("<textarea>").val(text);      /*.addClass("form-control")  removed after ":<textarea>"*/
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+
+
+  $("#3").on("blur", "textarea", function(){
+
+     //gets text, and rewrites it to text
+     var gettext =$("textarea").val();
+     text =gettext
+
+  
+  //sets up paragraph
+   var taskP = $("<p>").text(text);
+   $(this).replaceWith(taskP);
+
+   $("#3pm").on("click", function(){
+      window.localStorage.setItem("3pm",text)
+
+   })
+
+  });
+
+});
+
+
+//4PM Listener
+$("#4").on("click","p",function(){
+   var text = $(this).text().trim();
+
+   var textInput = $("<textarea>").val(text);      /*.addClass("form-control")  removed after ":<textarea>"*/
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+
+
+  $("#4").on("blur", "textarea", function(){
+
+     //gets text, and rewrites it to text
+     var gettext =$("textarea").val();
+     text =gettext
+
+  
+  //sets up paragraph
+   var taskP = $("<p>").text(text);
+   $(this).replaceWith(taskP);
+
+   $("#4pm").on("click", function(){
+      window.localStorage.setItem("4pm",text)
+
+   })
+
+  });
+
+});
+
+
+//5PM Listener
+$("#5").on("click","p",function(){
+   var text = $(this).text().trim();
+
+   var textInput = $("<textarea>").val(text);      /*.addClass("form-control")  removed after ":<textarea>"*/
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+
+
+  $("#5").on("blur", "textarea", function(){
+
+     //gets text, and rewrites it to text
+     var gettext =$("textarea").val();
+     text =gettext
+
+  
+  //sets up paragraph
+   var taskP = $("<p>").text(text);
+   $(this).replaceWith(taskP);
+
+   $("#5pm").on("click", function(){
+      window.localStorage.setItem("5pm",text)
+
+   })
+
+  });
+
+});
+
+
+//6PM Listener
+$("#6").on("click","p",function(){
+   var text = $(this).text().trim();
+
+   var textInput = $("<textarea>").val(text);      /*.addClass("form-control")  removed after ":<textarea>"*/
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+
+
+  $("#6").on("blur", "textarea", function(){
+
+     //gets text, and rewrites it to text
+     var gettext =$("textarea").val();
+     text =gettext
+
+  
+  //sets up paragraph
+   var taskP = $("<p>").text(text);
+   $(this).replaceWith(taskP);
+
+   $("#6pm").on("click", function(){
+      window.localStorage.setItem("6pm",text)
+
+   })
+
+  });
+
+});
+
+
+
+function loadTasks(){
+
+   //get 9am
+    var old9am =window.localStorage.getItem("9am");
+   
+   document.getElementById("nineam").innerHTML = old9am
+
+   //get 10am
+   var old10am =window.localStorage.getItem("10am");
+   
+   document.getElementById("tenam").innerText = old10am
+
+   //get 11am
+   var old11am =window.localStorage.getItem("11am");
+   
+   document.getElementById("elevenam").innerText = old11am
+
+   //get 12am
+
+   var old12pm =window.localStorage.getItem("12pm");
+   
+   document.getElementById("noon").innerText = old12pm
+   
+   //get 1pm
+
+   var old1pm =window.localStorage.getItem("1pm");
+   
+   document.getElementById("onepm").innerText = old1pm 
+
+   //get 2pm
+   var old2pm =window.localStorage.getItem("2pm");
+   
+   document.getElementById("twopm").innerText = old2pm
+
+   //get 3pm
+   var old3pm =window.localStorage.getItem("3pm");
+   
+   document.getElementById("threepm").innerText = old3pm
+
+   //get 4pm
+   var old4pm =window.localStorage.getItem("4pm");
+   
+   document.getElementById("fourpm").innerText = old4pm
+
+   //get 5pm 
+   var old5pm =window.localStorage.getItem("5pm");
+   
+   document.getElementById("fivepm").innerText = old5pm
+
+   //get 6pm 
+   var old6pm =window.localStorage.getItem("6pm");
+   
+   document.getElementById("sixpm").innerText = old6pm
+
+}
+
+
+
+
+
+
+loadTasks()
